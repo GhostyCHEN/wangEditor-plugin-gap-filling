86848b3c668751c2ee9eea05271a4873f5914166
yinjie.chen <1761038183@qq.com>
2024-02-23 11:22:02 +0800

    修复type不统一bug

--- src/module/parse-elem-html.js
+++ src/module/parse-elem-html.js
@@ -2,12 +2,12 @@ function parseHtml(elem, children, editor) {
   // 从 DOM element 中获取“附件”的信息
   const gapId = elem.getAttribute('gapId') || ''
   return {
-    type: 'GapFiliing',
+    type: 'gapFiliing',
     gapId,
   }
 }
 const parseHtmlConf = {
-  selector: 'span[data-w-e-type="GapFiliing"]', // data-w-e-type 属性，留给自定义元素，保证扩展性
+  selector: 'span[data-w-e-type="gapFiliing"]', // data-w-e-type 属性，留给自定义元素，保证扩展性
   parseElemHtml: parseHtml,
 }
 
